e3b62fc0e74ae58cb69cde304a89103f90699dec
Sir_u <ehdgur9553@naver.com>
2023-09-11 16:26:49 +0900

    modified a code

--- talkBot.py
+++ talkBot.py
@@ -1,22 +1,43 @@
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QMessageBox
 
-class Main(QDialog):
-   def __init__(self):
-      super().__init__()
-      self.init_ui()
-   
-   def init_ui(self):
-      main_layout = QVBoxLayout()
+class RecommendationApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-      layout_sample = QHBoxLayout()
+    def initUI(self):
+        # 모던한 디자인을 위해 스타일 시트를 사용
+        self.setStyleSheet("font-size: 18px; background-color: #f0f0f0;")
 
-      main_layout.addWidget(layout_sample)
+        # 추천 버튼 생성
+        self.recommend_button = QPushButton("추천해줘")
+        self.recommend_button.setStyleSheet("background-color: #3498db; color: white; padding: 10px; border: none;")
+        self.recommend_button.clicked.connect(self.showRecommendations)
 
-      self.setLayout(main_layout)
-      self.show()
+        # 텍스트 표시 레이블
+        self.text_label = QLabel("")
+
+        # 레이아웃 설정
+        layout = QVBoxLayout()
+        layout.addWidget(self.recommend_button)
+        layout.addWidget(self.text_label)
+        self.setLayout(layout)
+
+        # 창 설정
+        self.setWindowTitle("추천 앱")
+        self.setGeometry(100, 100, 400, 200)
+
+    def showRecommendations(self):
+        # 추천 버튼을 클릭했을 때 텍스트 3가지를 표시하고 선택할 수 있는 대화상자 표시
+        options = ["옵션 1", "옵션 2", "옵션 3"]
+        choice, _ = QMessageBox.getItemChooice(self, "추천 선택", "텍스트를 선택하세요:", options=options)
+        
+        if choice:
+            self.text_label.setText(f"선택한 텍스트: {choice}")
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   main = Main()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = RecommendationApp()
+    ex.show()
+    sys.exit(app.exec_())
