ecc28e9b6c81ca8c57cbfb4aa9f8ae2feb057789
Siru <ehdgur9553@naver.com>
2023-10-30 16:26:39 +0900

    의도 분류 모델

---
+++ DeepLearning/Preprocess_test.py
@@ -0,0 +1,13 @@
+from utils.Preprocess import Preprocess
+
+sent = "내일 오전 10시에 탕수육 주무하고 싶어"
+
+p = Preprocess(userdic='../utils/user_dic.tsv')
+
+pos = p.pos(sent)
+
+ret = p.get_keyword(pos, without_tag=False)
+print(ret)
+
+ret = p.get_keyword(pos, without_tag=True)
+print(ret)
