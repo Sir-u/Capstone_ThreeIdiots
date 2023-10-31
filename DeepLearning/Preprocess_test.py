132c9830ac3cbf491d191da1caf7903fbd135444
Siru <ehdgur9553@naver.com>
2023-10-31 11:35:01 +0900

    solved Import Error

--- DeepLearning/Preprocess_test.py
+++ DeepLearning/Preprocess_test.py
@@ -1,4 +1,8 @@
-From utils.Preprocess import Preprocess
+import sys
+sys.path.append('/Users/siru/GitHub/Capstone_ThreeIdiots/DeepLearning/utils/')
+from Preprocess import Preprocess
+
+# from utils.Preprocess import Preprocess
 
 sent = "내일 오전 10시에 탕수육 주무하고 싶어"
 
@@ -6,8 +10,8 @@ p = Preprocess(userdic='../utils/user_dic.tsv')
 
 pos = p.pos(sent)
 
-ret = p.get_keyword(pos, without_tag=False)
+ret = p.get_keywords(pos, without_tag=False)
 print(ret)
 
-ret = p.get_keyword(pos, without_tag=True)
+ret = p.get_keywords(pos, without_tag=True)
 print(ret)
