1b2b73416273429d21bd26c74f661c90fcb757e2
sandhouse24 <koreaonlyreal@naver.com>
2023-11-13 17:19:59 +0900

    경로 수정

--- DeepLearning/test/Preprocess_test.py
+++ DeepLearning/test/Preprocess_test.py
@@ -1,17 +1,23 @@
 import sys
-sys.path.append('../DeepLearning/utils/')
+sys.path.append('../Capstone_ThreeIdiots/DeepLearning/utils/')
 from Preprocess import Preprocess
+from keras import preprocessing
 
-# from utils.Preprocess import Preprocess
-
-sent = "내일 오전 10시에 탕수육 주무하고 싶어"
-
-p = Preprocess(userdic='../user_dic.tsv')
+sent = "내일 오전 10시에 짬뽕 주문하고 싶어ㅋㅋ"
+p = Preprocess(word2index_dic='../train_tools/dict/chatbot_dict.bin',
+               userdic = '../utils/user_dic.tsv')
 
 pos = p.pos(sent)
+keywords = p.get_keywords(pos, without_tag=False)
 
-ret = p.get_keywords(pos, without_tag=False)
-print(ret)
+print(keywords)
 
-ret = p.get_keywords(pos, without_tag=True)
-print(ret)
+# w2i = p.get_wordidx_sequence(keywords)
+# sequences = [w2i]
+#
+# MAX_SEQ_LEN = 15    # 임베딩 벡터 크기
+# padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN, padding='post')
+#
+# print(keywords)
+# print(sequences)
+# print(padded_seqs)
