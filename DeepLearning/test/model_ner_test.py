3353de104adfdd2ec0cdbc5c6f9bee3d368525fe
turtle648 <turtle648@daum.net>
2023-11-15 00:52:46 +0900

    경로수정

---
+++ DeepLearning/test/model_ner_test.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('../Capstone_ThreeIdiots/DeepLearning/utils/')
+from Preprocess import Preprocess
+import sys
+sys.path.append('../Capstone_ThreeIdiots/DeepLearning/ner/')
+from NerModel import NerModel
+
+p = Preprocess(word2index_dic='../Capstone_ThreeIdiots/DeepLearning/train_tools/dict/chatbot_dict.bin',
+               userdic = '../Capstone_ThreeIdiots/DeepLearning/utils/user_dic.tsv')
+
+ner = NerModel(model_name='../Capstone_ThreeIdiots/DeepLearning/ner/ner_model.h5', proprocess=p)
+query = '오늘 오전 13시 2분에 탕수육 주문 하고 싶어요'
+predicts = ner.predict(query)
+tags = ner.predict_tags(query)
+print(predicts)
+print(tags)
